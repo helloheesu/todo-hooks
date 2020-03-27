a3376cd304649af7b587ff2154619be85bf0bdeb
Heesu Jung (helloheesu) <heesu.jung.developer@gmail.com>
2020-03-27 21:47:53 +0900

    :sparkles: hide list when data is empty

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import "todomvc-common/base.css";
 import "todomvc-app-css/index.css";
 
 const App = () => {
+  const [todoList] = useState([]);
+
   return (
     <>
       <section className="todoapp">
@@ -10,47 +12,51 @@ const App = () => {
           <h1>todos</h1>
           <input className="new-todo" placeholder="What needs to be done?" />
         </header>
-        <section className="main">
-          <input id="toggle-all" className="toggle-all" type="checkbox" />
-          <label htmlFor="toggle-all">Mark all as complete</label>
-          <ul className="todo-list">
-            <li className="completed">
-              <div className="view">
-                <input className="toggle" type="checkbox" checked />
-                <label>Taste JavaScript</label>
-                <button className="destroy"></button>
-              </div>
-              <input className="edit" value="Create a TodoMVC template" />
-            </li>
-            <li>
-              <div className="view">
-                <input className="toggle" type="checkbox" />
-                <label>Buy a unicorn</label>
-                <button className="destroy"></button>
-              </div>
-              <input className="edit" value="Rule the web" />
-            </li>
-          </ul>
-        </section>
-        <footer className="footer">
-          <span className="todo-count">
-            <strong>0</strong> item left
-          </span>
-          <ul className="filters">
-            <li>
-              <a className="selected" href="#/">
-                All
-              </a>
-            </li>
-            <li>
-              <a href="#/active">Active</a>
-            </li>
-            <li>
-              <a href="#/completed">Completed</a>
-            </li>
-          </ul>
-          <button className="clear-completed">Clear completed</button>
-        </footer>
+        {todoList.length ? (
+          <>
+            <section className="main">
+              <input id="toggle-all" className="toggle-all" type="checkbox" />
+              <label htmlFor="toggle-all">Mark all as complete</label>
+              <ul className="todo-list">
+                <li className="completed">
+                  <div className="view">
+                    <input className="toggle" type="checkbox" checked />
+                    <label>Taste JavaScript</label>
+                    <button className="destroy"></button>
+                  </div>
+                  <input className="edit" value="Create a TodoMVC template" />
+                </li>
+                <li>
+                  <div className="view">
+                    <input className="toggle" type="checkbox" />
+                    <label>Buy a unicorn</label>
+                    <button className="destroy"></button>
+                  </div>
+                  <input className="edit" value="Rule the web" />
+                </li>
+              </ul>
+            </section>
+            <footer className="footer">
+              <span className="todo-count">
+                <strong>0</strong> item left
+              </span>
+              <ul className="filters">
+                <li>
+                  <a className="selected" href="#/">
+                    All
+                  </a>
+                </li>
+                <li>
+                  <a href="#/active">Active</a>
+                </li>
+                <li>
+                  <a href="#/completed">Completed</a>
+                </li>
+              </ul>
+              <button className="clear-completed">Clear completed</button>
+            </footer>
+          </>
+        ) : null}
       </section>
       <footer className="info">
         <p>Double-click to edit a todo</p>
