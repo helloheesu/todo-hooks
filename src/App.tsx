027436087c14d6e48f93afe88d7bdd82601875cd
Heesu Jung (helloheesu) <heesu.jung.developer@gmail.com>
2020-03-28 15:33:43 +0900

    :sparkles: show item list

--- src/App.tsx
+++ src/App.tsx
@@ -44,27 +44,29 @@ const App = () => {
               <input id="toggle-all" className="toggle-all" type="checkbox" />
               <label htmlFor="toggle-all">Mark all as complete</label>
               <ul className="todo-list">
-                <li className="completed">
+                {todoList.map(todo => (
+                  <li>
+                    <div className="view">
+                      <input className="toggle" type="checkbox" />
+                      <label>{todo}</label>
+                      <button className="destroy"></button>
+                    </div>
+                    <input className="edit" value={todo} />
+                  </li>
+                ))}
+                {/* <li className="completed">
                   <div className="view">
                     <input className="toggle" type="checkbox" checked />
                     <label>Taste JavaScript</label>
                     <button className="destroy"></button>
                   </div>
                   <input className="edit" value="Create a TodoMVC template" />
-                </li>
-                <li>
-                  <div className="view">
-                    <input className="toggle" type="checkbox" />
-                    <label>Buy a unicorn</label>
-                    <button className="destroy"></button>
-                  </div>
-                  <input className="edit" value="Rule the web" />
-                </li>
+                </li> */}
               </ul>
             </section>
             <footer className="footer">
               <span className="todo-count">
-                <strong>0</strong> item left
+                <strong>{todoList.length}</strong> item left
               </span>
               <ul className="filters">
                 <li>
